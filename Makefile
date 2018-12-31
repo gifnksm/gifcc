df72aaba4fb7ca544ec86019b2413807315006ff
gifnksm <makoto.nksm+github@gmail.com>
2018-12-31 18:00:59 +0900

    整数1つをコンパイルするコンパイラを作成

---
+++ Makefile
@@ -0,0 +1,9 @@
+9cc: 9cc.c
+
+test: 9cc
+	./test.sh
+
+clean:
+	rm -f 9cc *.o *~ tmp*
+
+
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+try() {
+  expected="$1"
+  input="$2"
+
+  ./9cc "${input}" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "${actual}" = "${expected}" ]; then
+    echo "${input} => ${actual}"
+  else
+    echo "${expected} expected, but got ${actual}"
+    exit 1
+  fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
