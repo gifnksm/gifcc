4d26422cc0c8b36803fff328d53e30767aa707d0
gifnksm <makoto.nksm+github@gmail.com>
2019-01-01 16:38:16 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -3,14 +3,34 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "引数の数が正しくありません\n");
+    fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
+
+  char *p = argv[1];
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
