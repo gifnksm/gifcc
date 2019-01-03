ad934506d2ac7bfd403b5f158d85c518acd5c814
gifnksm <makoto.nksm+github@gmail.com>
2019-01-04 00:13:24 +0900

    トークン、ASTを出力するモードを追加

--- main.c
+++ main.c
@@ -16,16 +16,101 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+static void output_token(void) {
+  for (int pos = 0; true; pos++) {
+    Token *token = get_token(pos);
+
+    if (token->ty <= 255) {
+      printf("%03d [%c]\n", pos, token->ty);
+      continue;
+    }
+
+    switch (token->ty) {
+    case TK_NUM:
+      printf("%03d NUM %d\n", pos, token->val);
+      break;
+    case TK_IDENT:
+      printf("%03d IDENT %s\n", pos, token->name);
+      break;
+    case TK_EQEQ:
+      printf("%03d [==]\n", pos);
+      break;
+    case TK_NOTEQ:
+      printf("%03d [!=]\n", pos);
+      break;
+    case TK_EOF:
+      printf("%03d EOF\n", pos);
+      break;
+    }
+
+    if (token->ty == TK_EOF) {
+      break;
+    }
+  }
+}
+
+static void dump_node(Node *node, int level);
+static void dump_complex_node(Node *node, char *label, int level) {
+  printf("%*s(%s\n", 2 * level, "", label);
+  if (node->lhs != NULL)
+    dump_node(node->lhs, level + 1);
+  if (node->rhs != NULL)
+    dump_node(node->rhs, level + 1);
+  printf("%*s)\n", 2 * level, "");
+}
+
+static void dump_node(Node *node, int level) {
+  if (node->ty <= 255) {
+    dump_complex_node(node, (char[]){'[', node->ty, ']', '\0'}, level + 1);
+    return;
+  }
+
+  switch (node->ty) {
+  case ND_NUM:
+    printf("%*s(NUM %d)\n", 2 * level, "", node->val);
+    break;
+  case ND_IDENT:
+    printf("%*s(IDENT %s)\n", 2 * level, "", node->name);
+    break;
+  case ND_EQEQ:
+    dump_complex_node(node, "[==]", level + 1);
+    break;
+  case ND_NOTEQ:
+    dump_complex_node(node, "[!=]", level + 1);
+    break;
+  }
+}
+
+static void output_ast(void) {
+  for (int pos = 0; true; pos++) {
+    Node *node = get_node(pos);
+    if (node == NULL) {
+      break;
+    }
+
+    dump_node(node, 0);
+  }
+}
+
 enum {
   OPTVAL_TEST = 256,
+  OPTVAL_OUTPUT,
 };
+
+typedef enum {
+  OUTPUT_ASM,
+  OUTPUT_TOKEN,
+  OUTPUT_AST,
+} output_t;
 
 struct option longopts[] = {
     {"test", no_argument, NULL, OPTVAL_TEST},
+    {"output", required_argument, NULL, OPTVAL_OUTPUT},
     {NULL, 0, 0, 0},
 };
 
 int main(int argc, char **argv) {
+  output_t output_mode = OUTPUT_ASM;
   while (true) {
     int c = getopt_long(argc, argv, "", longopts, NULL);
     if (c == -1)
@@ -35,6 +120,18 @@ int main(int argc, char **argv) {
     case OPTVAL_TEST:
       runtest();
       return 0;
+    case OPTVAL_OUTPUT:
+      if (strcmp(optarg, "asm") == 0) {
+        output_mode = OUTPUT_ASM;
+      } else if (strcmp(optarg, "token") == 0) {
+        output_mode = OUTPUT_TOKEN;
+      } else if (strcmp(optarg, "ast") == 0) {
+        output_mode = OUTPUT_AST;
+      } else {
+        error("不明なオプションの値です: %s", optarg);
+        return 1;
+      }
+      break;
     case '?':
       return 1;
     }
@@ -49,7 +146,16 @@ int main(int argc, char **argv) {
   // トークナイズしてパースする
   // 結果はcodeに保存される
   tokenize(input);
+  if (output_mode == OUTPUT_TOKEN) {
+    output_token();
+    return 0;
+  }
+
   program();
+  if (output_mode == OUTPUT_AST) {
+    output_ast();
+    return 0;
+  }
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
