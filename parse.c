b730affb229e375a077c4bbef10809f795ed163f
gifnksm <makoto.nksm+github@gmail.com>
2019-01-04 23:24:07 +0900

    BNF 定義に合わせてパーサーの関数名を変更

--- parse.c
+++ parse.c
@@ -3,11 +3,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-static Node *assign(void);
-static Node *equal(void);
-static Node *add(void);
-static Node *mul(void);
-static Node *term(void);
+static Node *assignment_expression(void);
+static Node *equality_expression(void);
+static Node *addtive_expression(void);
+static Node *multiplicative_expression(void);
+static Node *primary_expression(void);
 
 static int pos = 0;
 static Vector *code;
@@ -58,63 +58,63 @@ void program(void) {
   stack_map = new_map();
 
   while (get_token(pos)->ty != TK_EOF) {
-    vec_push(code, assign());
+    vec_push(code, assignment_expression());
     while (consume(';'))
       ;
   }
   vec_push(code, NULL);
 }
 
-static Node *assign(void) {
-  Node *lhs = equal();
+static Node *assignment_expression(void) {
+  Node *lhs = equality_expression();
   if (consume('='))
-    return new_node('=', lhs, assign());
+    return new_node('=', lhs, assignment_expression());
   return lhs;
 }
 
-static Node *equal(void) {
-  Node *node = add();
+static Node *equality_expression(void) {
+  Node *node = addtive_expression();
   while (true) {
     if (consume(TK_EQEQ))
-      node = new_node(ND_EQEQ, node, add());
+      node = new_node(ND_EQEQ, node, addtive_expression());
     else if (consume(TK_NOTEQ))
-      node = new_node(ND_NOTEQ, node, add());
+      node = new_node(ND_NOTEQ, node, addtive_expression());
     else
       return node;
   }
 }
 
-static Node *add(void) {
-  Node *node = mul();
+static Node *addtive_expression(void) {
+  Node *node = multiplicative_expression();
   while (true) {
     if (consume('+'))
-      node = new_node('+', node, mul());
+      node = new_node('+', node, multiplicative_expression());
     else if (consume('-'))
-      node = new_node('-', node, mul());
+      node = new_node('-', node, multiplicative_expression());
     else
       return node;
   }
 }
 
-static Node *mul(void) {
-  Node *node = term();
+static Node *multiplicative_expression(void) {
+  Node *node = primary_expression();
   while (true) {
     if (consume('*'))
-      node = new_node('*', node, term());
+      node = new_node('*', node, primary_expression());
     else if (consume('/'))
-      node = new_node('/', node, term());
+      node = new_node('/', node, primary_expression());
     else
       return node;
   }
 }
 
-static Node *term(void) {
+static Node *primary_expression(void) {
   if (get_token(pos)->ty == TK_NUM)
     return new_node_num(get_token(pos++)->val);
   if (get_token(pos)->ty == TK_IDENT)
     return new_node_ident(get_token(pos++)->name);
   if (consume('(')) {
-    Node *node = assign();
+    Node *node = assignment_expression();
     if (!consume(')'))
       error("開きカッコに対応する閉じカッコがありません: %s",
             get_token(pos)->input);
