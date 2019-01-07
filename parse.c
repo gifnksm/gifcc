7f8a768f24f791dc772ba3d9e3c0ec960d1a7fa7
gifnksm <makoto.nksm+github@gmail.com>
2019-01-07 17:02:57 +0900

    パーサーの関数の並びを n1548 Annex.A の順番と同じにする

--- parse.c
+++ parse.c
@@ -3,14 +3,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-static Node *expression(void);
-static Node *assignment_expression(void);
-static Node *equality_expression(void);
-static Node *addtive_expression(void);
-static Node *multiplicative_expression(void);
+// expression
+static Node *primary_expression(void);
 static Node *postfix_expression(void);
 static Vector *argument_expression_list(void);
-static Node *primary_expression(void);
+static Node *multiplicative_expression(void);
+static Node *addtive_expression(void);
+static Node *equality_expression(void);
+static Node *assignment_expression(void);
+static Node *expression(void);
 
 static int pos = 0;
 static Vector *code;
@@ -76,49 +77,19 @@ void program(void) {
   vec_push(code, NULL);
 }
 
-static Node *expression(void) { return assignment_expression(); }
-
-static Node *assignment_expression(void) {
-  Node *lhs = equality_expression();
-  if (consume('='))
-    return new_node('=', lhs, assignment_expression());
-  return lhs;
-}
-
-static Node *equality_expression(void) {
-  Node *node = addtive_expression();
-  while (true) {
-    if (consume(TK_EQEQ))
-      node = new_node(ND_EQEQ, node, addtive_expression());
-    else if (consume(TK_NOTEQ))
-      node = new_node(ND_NOTEQ, node, addtive_expression());
-    else
-      return node;
+static Node *primary_expression(void) {
+  if (get_token(pos)->ty == TK_NUM)
+    return new_node_num(get_token(pos++)->val);
+  if (get_token(pos)->ty == TK_IDENT)
+    return new_node_ident(get_token(pos++)->name);
+  if (consume('(')) {
+    Node *node = expression();
+    if (!consume(')'))
+      error("開きカッコに対応する閉じカッコがありません: %s",
+            get_token(pos)->input);
+    return node;
   }
-}
-
-static Node *addtive_expression(void) {
-  Node *node = multiplicative_expression();
-  while (true) {
-    if (consume('+'))
-      node = new_node('+', node, multiplicative_expression());
-    else if (consume('-'))
-      node = new_node('-', node, multiplicative_expression());
-    else
-      return node;
-  }
-}
-
-static Node *multiplicative_expression(void) {
-  Node *node = postfix_expression();
-  while (true) {
-    if (consume('*'))
-      node = new_node('*', node, postfix_expression());
-    else if (consume('/'))
-      node = new_node('/', node, postfix_expression());
-    else
-      return node;
-  }
+  error("数値でも開きカッコでもないトークンです: %s", get_token(pos)->input);
 }
 
 static Node *postfix_expression(void) {
@@ -150,17 +121,47 @@ static Vector *argument_expression_list(void) {
   return argument;
 }
 
-static Node *primary_expression(void) {
-  if (get_token(pos)->ty == TK_NUM)
-    return new_node_num(get_token(pos++)->val);
-  if (get_token(pos)->ty == TK_IDENT)
-    return new_node_ident(get_token(pos++)->name);
-  if (consume('(')) {
-    Node *node = expression();
-    if (!consume(')'))
-      error("開きカッコに対応する閉じカッコがありません: %s",
-            get_token(pos)->input);
-    return node;
+static Node *multiplicative_expression(void) {
+  Node *node = postfix_expression();
+  while (true) {
+    if (consume('*'))
+      node = new_node('*', node, postfix_expression());
+    else if (consume('/'))
+      node = new_node('/', node, postfix_expression());
+    else
+      return node;
   }
-  error("数値でも開きカッコでもないトークンです: %s", get_token(pos)->input);
 }
+
+static Node *addtive_expression(void) {
+  Node *node = multiplicative_expression();
+  while (true) {
+    if (consume('+'))
+      node = new_node('+', node, multiplicative_expression());
+    else if (consume('-'))
+      node = new_node('-', node, multiplicative_expression());
+    else
+      return node;
+  }
+}
+
+static Node *equality_expression(void) {
+  Node *node = addtive_expression();
+  while (true) {
+    if (consume(TK_EQEQ))
+      node = new_node(ND_EQEQ, node, addtive_expression());
+    else if (consume(TK_NOTEQ))
+      node = new_node(ND_NOTEQ, node, addtive_expression());
+    else
+      return node;
+  }
+}
+
+static Node *assignment_expression(void) {
+  Node *lhs = equality_expression();
+  if (consume('='))
+    return new_node('=', lhs, assignment_expression());
+  return lhs;
+}
+
+static Node *expression(void) { return assignment_expression(); }
