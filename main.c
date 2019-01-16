1eea6b010fbbd0041a21a50f050eda3f8324d715
gifnksm <makoto.nksm+github@gmail.com>
2019-01-16 23:49:23 +0900

    ASTダンプで式の型も表示する

--- main.c
+++ main.c
@@ -148,20 +148,45 @@ static void output_token(void) {
   }
 }
 
+static void dump_indent(int level) { printf("%*s", 2 * level, ""); }
+static void dump_type_inner(Type *ty) {
+  switch (ty->ty) {
+  case TY_INT:
+    printf("int");
+    break;
+  case TY_PTR:
+    printf("PTR(");
+    dump_type_inner(ty->ptrof);
+    printf(")");
+    break;
+  default:
+    error("未知の型です: %d\n", ty->ty);
+  }
+}
+static void dump_type(Type *ty) {
+  printf("<");
+  dump_type_inner(ty);
+  printf(">");
+}
 static void dump_expr(Expr *expr, int level);
 static void dump_binop_expr(Expr *expr, char *label, int level) {
-  printf("%*s(%s\n", 2 * level, "", label);
+  dump_indent(level);
+  dump_type(expr->val_type);
+  printf("(%s\n", label);
   if (expr->lhs != NULL) {
     dump_expr(expr->lhs, level + 1);
   } else {
-    printf("%*s(NULL)\n", 2 * (level + 1), "");
+    dump_indent(level + 1);
+    printf("<void>(NULL)\n");
   }
   if (expr->rhs != NULL) {
     dump_expr(expr->rhs, level + 1);
   } else {
-    printf("%*s(NULL)\n", 2 * (level + 1), "");
+    dump_indent(level + 1);
+    printf("<void>(NULL)\n");
   }
-  printf("%*s)\n", 2 * level, "");
+  dump_indent(level);
+  printf(")\n");
 }
 
 static void dump_expr(Expr *expr, int level) {
@@ -172,10 +197,14 @@ static void dump_expr(Expr *expr, int level) {
 
   switch (expr->ty) {
   case EX_NUM:
-    printf("%*s(NUM %d)\n", 2 * level, "", expr->val);
+    dump_indent(level);
+    dump_type(expr->val_type);
+    printf("(NUM %d)\n", expr->val);
     break;
   case EX_IDENT:
-    printf("%*s(IDENT %s)\n", 2 * level, "", expr->name);
+    dump_indent(level);
+    dump_type(expr->val_type);
+    printf("(IDENT %s)\n", expr->name);
     break;
   case EX_EQEQ:
     dump_binop_expr(expr, "[==]", level);
@@ -202,11 +231,14 @@ static void dump_expr(Expr *expr, int level) {
     dump_binop_expr(expr, "[||]", level);
     break;
   case EX_COND:
-    printf("%*s(COND\n", 2 * level, "");
+    dump_indent(level);
+    dump_type(expr->val_type);
+    printf("(COND\n");
     dump_expr(expr->cond, level + 1);
     dump_expr(expr->lhs, level + 1);
     dump_expr(expr->rhs, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf(")\n");
     break;
   case EX_INC:
     dump_binop_expr(expr, "[++]", level);
@@ -245,14 +277,17 @@ static void dump_expr(Expr *expr, int level) {
     dump_binop_expr(expr, "[^=]", level);
     break;
   case EX_CALL:
-    printf("%*s(CALL\n", 2 * level, "");
+    dump_indent(level);
+    dump_type(expr->val_type);
+    printf("(CALL\n");
     dump_expr(expr->callee, level + 1);
     if (expr->argument != NULL) {
       for (int i = 0; i < expr->argument->len; i++) {
         dump_expr(expr->argument->data[i], level + 1);
       }
     }
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf(")\n");
     break;
   default:
     error("未知のノードです: %d\n", expr->ty);
@@ -262,89 +297,116 @@ static void dump_expr(Expr *expr, int level) {
 static void dump_stmt(Stmt *stmt, int level) {
   switch (stmt->ty) {
   case ST_EXPR:
-    printf("%*s(EXPR\n", 2 * level, "");
+    dump_indent(level);
+    printf("{EXPR\n");
     dump_expr(stmt->expr, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("}\n");
     break;
   case ST_COMPOUND:
-    printf("%*s(COMPOUND\n", 2 * level, "");
+    dump_indent(level);
+    printf("{COMPOUND\n");
     for (int i = 0; i < stmt->stmts->len; i++) {
       dump_stmt(stmt->stmts->data[i], level + 1);
     }
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("}\n");
     break;
   case ST_IF:
-    printf("%*s(IF\n", 2 * level, "");
+    dump_indent(level);
+    printf("{IF\n");
     dump_expr(stmt->cond, level + 1);
     dump_stmt(stmt->then_stmt, level + 1);
     dump_stmt(stmt->else_stmt, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_SWITCH:
-    printf("%*s(SWITCH\n", 2 * level, "");
+    dump_indent(level);
+    printf("{SWITCH\n");
     dump_expr(stmt->cond, level + 1);
     dump_stmt(stmt->body, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_CASE:
-    printf("%*s(CASE\n", 2 * level, "");
+    dump_indent(level);
+    printf("{CASE\n");
     dump_expr(stmt->expr, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_DEFAULT:
-    printf("%*s(DEFAULT)\n", 2 * level, "");
+    dump_indent(level);
+    printf("{DEFAULT}\n");
     break;
   case ST_LABEL:
-    printf("%*s(LABEL %s)\n", 2 * level, "", stmt->name);
+    dump_indent(level);
+    printf("{LABEL %s}\n", stmt->name);
     break;
   case ST_WHILE:
-    printf("%*s(WHILE\n", 2 * level, "");
+    dump_indent(level);
+    printf("{WHILE\n");
     dump_expr(stmt->cond, level + 1);
     dump_stmt(stmt->body, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_DO_WHILE:
-    printf("%*s(DO_WHILE\n", 2 * level, "");
+    dump_indent(level);
+    printf("{DO_WHILE\n");
     dump_expr(stmt->cond, level + 1);
     dump_stmt(stmt->body, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_FOR:
-    printf("%*s(FOR\n", 2 * level, "");
+    dump_indent(level);
+    printf("{FOR\n");
     if (stmt->init != NULL) {
       dump_expr(stmt->init, level + 1);
     } else {
-      printf("%*s(NULL)\n", 2 * (level + 1), "");
+      dump_indent(level + 1);
+      printf("<void>(NULL)\n");
     }
     if (stmt->cond != NULL) {
       dump_expr(stmt->cond, level + 1);
     } else {
-      printf("%*s(NULL)\n", 2 * (level + 1), "");
+      dump_indent(level + 1);
+      printf("<void>(NULL)\n");
     }
     if (stmt->inc != NULL) {
       dump_expr(stmt->inc, level + 1);
     } else {
-      printf("%*s(NULL)\n", 2 * (level + 1), "");
+      dump_indent(level + 1);
+      printf("<void>(NULL)\n");
     }
     dump_stmt(stmt->body, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_GOTO:
-    printf("%*s(GOTO %s)\n", 2 * level, "", stmt->name);
+    dump_indent(level);
+    printf("{GOTO %s}\n", stmt->name);
     break;
   case ST_BREAK:
-    printf("%*s(BREAK)\n", 2 * level, "");
+    dump_indent(level);
+    printf("{BREAK}\n");
     break;
   case ST_CONTINUE:
-    printf("%*s(CONTINUE)\n", 2 * level, "");
+    dump_indent(level);
+    printf("{CONTINUE}\n");
     break;
   case ST_RETURN:
-    printf("%*s(RETURN\n", 2 * level, "");
+    dump_indent(level);
+    printf("{RETURN\n");
     dump_expr(stmt->expr, level + 1);
-    printf("%*s)\n", 2 * level, "");
+    dump_indent(level);
+    printf("\n");
     break;
   case ST_NULL:
-    printf("%*s(NULL)\n", 2 * level, "");
+    dump_indent(level);
+    printf("{NULL}\n");
     break;
   default:
     error("未知のノードです: %d\n", stmt->ty);
@@ -353,9 +415,11 @@ static void dump_stmt(Stmt *stmt, int level) {
 
 static void output_ast(Function *func) {
   int level = 0;
-  printf("%*s(FUNCTION %s\n", 2 * level, "", func->name);
+  dump_indent(level);
+  printf("{FUNCTION %s\n", func->name);
   dump_stmt(func->body, level + 1);
-  printf("%*s)\n", 2 * level, "");
+  dump_indent(level);
+  printf(")\n");
 }
 
 enum {
@@ -422,8 +486,8 @@ int main(int argc, char **argv) {
   if (output_mode == OUTPUT_AST) {
     for (int i = 0; i < func_list->len; i++) {
       output_ast(func_list->data[i]);
-      return 0;
     }
+    return 0;
   }
 
   printf(".intel_syntax noprefix\n");
